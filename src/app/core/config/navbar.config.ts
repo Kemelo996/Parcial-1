19cdc10d7ad329350be1e3f4000285de871eefeb
Sebastian-Henao <sebastian.henao03@uceva.edu.co>
2026-09-02 21:02:49 -0500

    fix(develop): se elimina usuarios de navbar

--- src/app/core/config/navbar.config.ts
+++ src/app/core/config/navbar.config.ts
@@ -15,6 +15,5 @@ import { NavbarItem } from "../../modules/shared/interfaces/navbar.interface";
  */
 export const NAVBAR_ITEMS: NavbarItem[] = [
     { label: 'Arquitectura', url: '/arquitectura' },
-    { label: 'Usuarios', url: '/users' },
     { label: 'Principios', url: '/principios' },
 ];
